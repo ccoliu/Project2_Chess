d94567c60eed58f10f6fd359ee983781bf79f14d
0423brian <130418761+0423brian@users.noreply.github.com>
2023-05-06 16:31:07 +0800

    Update King.h
    king

--- Chess/King.h
+++ Chess/King.h
@@ -1,1 +1,34 @@
 #pragma once
+#include "ChessMan.h"
+#include <vector>
+using namespace std;
+
+class King : public ChessMan
+{
+public:
+	//initialize the queen
+	King(Color col, Position position) : ChessMan(col, position)
+	{
+		if (col == Color::white)
+		{
+			this->icon = 'K';
+		}
+		else
+		{
+			this->icon = 'q';
+		}
+	};
+	vector<Position> Move(Position newPosition) override
+	{
+		vector<Position> accessable;
+		accessable.push_back(Position(position.y + 1, position.x + 1));
+		accessable.push_back(Position(position.y + 1, position.x - 1));
+		accessable.push_back(Position(position.y - 1, position.x + 1));
+		accessable.push_back(Position(position.y - 1, position.x - 1));
+		accessable.push_back(Position(position.y + 1, position.x));
+		accessable.push_back(Position(position.y - 1, position.x));
+		accessable.push_back(Position(position.y, position.x + 1));
+		accessable.push_back(Position(position.y, position.x - 1));
+		return accessable;
+	}
+};
